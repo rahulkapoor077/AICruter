51fa8905c3e8d35ffed80a728dc5460cc111c902
rahulkapoor077 <117432576+rahulkapoor077@users.noreply.github.com>
2025-08-12 00:12:05 +0530

    Add files via upload

---
+++ layout.jsx
@@ -0,0 +1,27 @@
+"use client"; // makes this whole layout a client component
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Provider from "@/app/Provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
